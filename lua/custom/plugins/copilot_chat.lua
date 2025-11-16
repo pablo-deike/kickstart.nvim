e8eda420e37f51af0798767731b55a2cd3e29bb7
pablo deike <pablodeike12@gmail.com>
2025-11-16 19:37:59 +0100

    new copilot extension

--- lua/custom/plugins/copilot_chat.lua
+++ lua/custom/plugins/copilot_chat.lua
@@ -2,17 +2,21 @@ return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
-      { 'nvim-lua/plenary.nvim' },
+      { 'github/copilot.vim' },
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
     },
-    config = function()
-      require('CopilotChat').setup {
-        debug = false, -- set true if you want logs
-      }
+    build = 'make tiktoken',
+    opts = {},
 
-      -- Example keymaps
-      vim.api.nvim_set_keymap('n', '<leader>cc', ':CopilotChat<CR>', { desc = 'Open Copilot Chat' })
-      vim.api.nvim_set_keymap('v', '<leader>ce', ':CopilotChatExplain<CR>', { desc = 'Explain code', noremap = true })
-      vim.api.nvim_set_keymap('v', '<leader>cf', ':CopilotChatFix<CR>', { desc = 'Fix code', noremap = true })
-    end,
+    keys = {
+      { '<leader>zc', ':CopilotChat<CR>', mode = 'n', desc = 'Chat with copilot' },
+      { '<leader>ze', ':CopilotChatExplain<CR>:', mode = 'v', desc = 'Explain code' },
+      { '<leader>zr', ':CopilotChatReview<CR>', mode = 'v', desc = 'Review code' },
+      { '<leader>zf', ':CopilotChatFix<CR>', mode = 'v', desc = 'Fix Code Issues' },
+      { '<leader>zo', ':CopilotChatOptimize<CR>', mode = 'v', desc = 'Optimize Code' },
+      { '<leader>zd', ':CopilotChatDocs<CR>', mode = 'v', desc = 'Generate Documentation' },
+      { '<leader>zt', ':CopilotChatTests<CR>', mode = 'v', desc = 'Generate Tests' },
+      { '<leader>zm', ':CopilotChatCommit<CR>', mode = 'v', desc = 'Generate Commit Message' },
+    },
   },
 }
